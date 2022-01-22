d2123f1503cba2121c94f174b14537947e1de0a7
sahil-awasthi <sahil01.awasthi@gmail.com>
2022-01-22 11:32:59 +0530

    Changed API KEY in config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '7889936c-0907-4cd4-919a-e7a9e7c63425';
+export const KEY = 'f5da8d1b-0a9c-4011-afc9-192304241959';
 export const MODEL_CLOSE_SEC = 2.5;
